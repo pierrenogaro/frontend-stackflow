04cb3bdfb01bc1c7d2ece914c11b4e8fa4e914a7
Pierre <pierre.nogaro@gmail.com>
2025-02-23 10:50:23 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1 class="text-primary">Test Bootstrap</h1>
+    <button class="btn btn-success">Click Me</button>
+  </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
